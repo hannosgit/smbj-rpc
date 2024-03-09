d66e81431a17d6d88be98c527a2d9f292a20488f
Hannes Oswald <hannes.oswald97@gmail.com>
2024-03-09 22:34:45 +0100

    add test for getShares0

--- src/integration-test/java/com/rapid7/ServerServiceIntegrationTests.java
+++ src/integration-test/java/com/rapid7/ServerServiceIntegrationTests.java
@@ -1,5 +1,6 @@
 package com.rapid7;
 
+import com.google.common.collect.Lists;
 import com.hierynomus.mssmb2.SMB2Dialect;
 import com.hierynomus.security.bc.BCSecurityProvider;
 import com.hierynomus.smbj.SMBClient;
@@ -7,6 +8,7 @@ import com.hierynomus.smbj.SmbConfig;
 import com.hierynomus.smbj.connection.Connection;
 import com.hierynomus.smbj.session.Session;
 import com.rapid7.client.dcerpc.mssrvs.ServerService;
+import com.rapid7.client.dcerpc.mssrvs.dto.NetShareInfo0;
 import com.rapid7.client.dcerpc.mssrvs.dto.NetShareInfo502;
 import com.rapid7.client.dcerpc.transport.RPCTransport;
 import com.rapid7.client.dcerpc.transport.SMBTransportFactories;
@@ -16,8 +18,11 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
@@ -25,6 +30,27 @@ public class ServerServiceIntegrationTests {
 
     @Container
     private static final GenericContainer<?> sambaContainer = SambaContainer.create();
+
+    private static final ArrayList<String> expectedShareNames = Lists.newArrayList("public", "readonly", "dfs", "user", "IPC$");
+
+    @Test
+    void testGetShares0() throws IOException
+    {
+        final SmbConfig smbConfig = SmbConfig.builder().withSecurityProvider(new BCSecurityProvider()).withDialects(SMB2Dialect.SMB_2_1).withEncryptData(false).build();
+        final SMBClient smbClient = new SMBClient(smbConfig);
+        try (
+                final Connection smbConnection = smbClient.connect("localhost", sambaContainer.getFirstMappedPort());
+                final Session session = smbConnection.authenticate(SambaContainer.getAuthenticationContext())
+        ) {
+            final RPCTransport transport = SMBTransportFactories.SRVSVC.getTransport(session);
+            final ServerService serverService = new ServerService(transport);
+
+            final List<NetShareInfo0> shares0 = serverService.getShares0();
+            final List<String> shareNames = shares0.stream().map(NetShareInfo0::getNetName).collect(Collectors.toList());
+
+            assertThat(shareNames).containsExactlyInAnyOrderElementsOf(expectedShareNames);
+        }
+    }
 
     @Test
     void testGetShares502() throws IOException
@@ -39,7 +65,10 @@ public class ServerServiceIntegrationTests {
             final ServerService serverService = new ServerService(transport);
 
             final List<NetShareInfo502> shares502 = serverService.getShares502();
-            assertEquals(5,shares502.size());
+            final List<String> shareNames = shares502.stream().map(NetShareInfo0::getNetName).collect(Collectors.toList());
+
+            assertThat(shareNames).containsExactlyInAnyOrderElementsOf(expectedShareNames);
+
             assertEquals(1, shares502.get(0).getSecurityDescriptor().getDacl().getAces().size());
         }
     }
